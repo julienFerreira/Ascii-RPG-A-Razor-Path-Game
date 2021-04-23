691d8b4260afc7bf4f5ab4111809332ecc55fc5e
Ravindranathan Rajinth <rajinth.ravindranathan@he-arc.ch>
2021-04-23 13:48:30 +0200

    Done : auth with google

--- testApp/Startup.cs
+++ testApp/Startup.cs
@@ -30,6 +30,15 @@ namespace ARPG
             //services.AddDbContext<ARPGContext>(options =>
                 //options.UseSqlServer(Configuration.GetConnectionString("ARPGContext")));
             services.AddRazorPages();
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    IConfigurationSection googleAuthNSection =
+                        Configuration.GetSection("Authentication:Google");
+
+                    options.ClientId = googleAuthNSection["ClientId"];
+                    options.ClientSecret = googleAuthNSection["ClientSecret"];
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
